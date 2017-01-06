147fcf30f44dc662b464393369e81b17704695c8
alfredo navas <elpuas@gmail.com>
2017-01-06 16:23:50 -0500

    second commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Enqueue Ion parent theme CSS file
+
+add_action( 'wp_enqueue_scripts', 'appp_ion_enqueue_styles' );
+function appp_ion_enqueue_styles() {
+
+	// parent theme css
+	$version = AppPresser_Ion_Theme_Setup::VERSION . '.' . filemtime( get_template_directory() . '/style.css' );
+    wp_enqueue_style( 'ion-style', get_template_directory_uri().'/style.css', null, $version );
+
+    // child theme css
+    wp_enqueue_style( 'ion-child-style', get_stylesheet_uri(), null, filemtime( get_stylesheet_directory() . '/style.css' ) );
+		wp_enqueue_style( 'material-style', get_stylesheet_directory_uri() . '/assets/css/main.min.css' );
+}
+
+// Add your custom functions here
