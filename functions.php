54225eeb4a6f7ec934efb293eea7fde103b09e93
alfredo navas <elpuas@gmail.com>
2017-01-10 20:17:55 -0500

    More Juice

--- functions.php
+++ functions.php
@@ -14,3 +14,29 @@ function appp_ion_enqueue_styles() {
 		wp_enqueue_style( 'ion-child-style', get_stylesheet_uri(), null, filemtime( get_stylesheet_directory() . '/style.css' ) );
 		wp_enqueue_style( 'material-style', get_stylesheet_directory_uri() . '/assets/css/main.min.css' );
 		}
+
+		// Add Cart Header
+		function my_header_add_to_cart_fragment( $fragments ) {
+
+		  ob_start();
+		  $count = WC()->cart->cart_contents_count;
+		  ?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
+		  if ( $count > 0 ) {
+		      ?>
+		      <span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+		      <?php
+		  }
+		      ?></a><?php
+
+		  $fragments['a.cart-contents'] = ob_get_clean();
+
+		  return $fragments;
+		}
+		add_filter( 'woocommerce_add_to_cart_fragments', 'my_header_add_to_cart_fragment' );
+
+
+
+
+
+
+		 ?>
---
+++ header.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package Ion
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<?php do_action('appp_after_body'); ?>
+
+<div id="body-container">
+
+		<div id="menu-content" class="menu-content pane menu-animated">
+
+			<header class="bar bar-header">
+
+				<div class="buttons">
+
+					<?php do_action( 'appp_header_left' ); ?>
+
+					<button id="nav-left-open" class="nav-left-btn button button-icon icon ion-navicon"></button>
+
+				</div>
+
+				<img src="https://www.appderma.com/wp-content/uploads/2017/01/0.png" class="logo-responsive" />
+
+				<div class="buttons">
+
+					<!-- Add Cart Icon -->
+					<?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+
+						$count = WC()->cart->cart_contents_count;
+							?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php
+						if ( $count > 0 ) {
+							?>
+						<span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+					<?php
+								}
+						?></a>
+
+					<?php } ?>
+
+				</div>
+
+			</header><!-- #masthead -->
+
+			<div id="page" class="hfeed site">
+				<?php do_action( 'appp_before' ); ?>
+
+				<div id="main" <?php body_class( 'site-main' ); ?>>
